147dc1dd20963ad340ce23b49050c707f4e429aa
jumag <asylbek@dyn-10-32-100-249.winet.local>
2016-02-08 11:36:00 +0000

    Initial commit
    Commit this

---
+++ src/tailspin/surveys/TheSurveysAppServlet.java
@@ -0,0 +1,13 @@
+package tailspin.surveys;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class TheSurveysAppServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
